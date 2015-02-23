ad11f5cc4168971d06dba63ffc40a1113269c798
Andy Miller <rhuk@mac.com>
2015-02-23 16:32:37 -0700

    switched to named jQuery asset + bootstrap asset collection

--- bootstrap.php
+++ bootstrap.php
@@ -5,5 +5,22 @@ use Grav\Common\Theme;
 
 class Bootstrap extends Theme
 {
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onAssetsInitialized' => ['onAssetsInitialized', 0],
+        ];
+    }
 
+    public function onAssetsInitialized()
+    {
+        if ($this->isAdmin()) {
+            $this->active = false;
+            return;
+        }
+
+        $assets = $this->grav['assets'];
+        $assets->registerCollection('bootstrap', ['theme://css/bootstrap.min.css', 'theme://js/bootstrap.min.js']);
+        $assets->add('bootstrap', 100);
+    }
 }
